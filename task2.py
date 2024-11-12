caa6b7c33d593ec1c663d7c101e419ae40490b25
MBukharov <mikhail.bukharov@gmail.com>
2024-11-12 18:54:25 +0300

    add shablon.html

--- task2.py
+++ task2.py
@@ -4,15 +4,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return render_template("index.html")
+    content = {
+        'active_page1' : 'active'
+    }
+    return render_template("index.html", **content)
 
 @app.route("/players")
 def players_page():
-    return render_template("players.html")
+    content = {
+        'active_page2': 'active'
+    }
+    return render_template("players.html", **content)
 
 @app.route("/contacts")
 def contact_page():
-    return render_template("contacts.html")
+    content = {
+        'active_page3': 'active'
+    }
+    return render_template("contacts.html", **content)
 
 if __name__ == "__main__":
     app.run()
